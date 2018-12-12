ff2328535c77e18e26aa940a91714fe10bb5b682
Manikanta <im.manikanta@gmail.com>
2018-12-12 20:59:22 +0800

    Bug/#9 fix package lock (#10)
    * #9 Fix package-lock
    remove unused imports from index.js
    
    * Adding travis generated token

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ serviceAccount.json is not committed to repo. Download it from firebase console.
 
 Deploy Instructions:
 firebase deploy --only functions
+
+# This generates a token, e.g. "1/AD7sdasdasdKJA824OvEFc1c89Xz2ilBlaBlaBla"
+`firebase login:ci`
+# Encrypt this token
+`travis encrypt "1/AD7sdasdasdKJA824OvEFc1c89Xz2ilBlaBlaBla" --add`
+# This command may generate a warning ("If you tried to pass the name of the repository as the first argument, you probably won't get the results you wanted"). You can ignore it.
--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,10 @@
 const functions = require('firebase-functions');
 const express = require('express');
-const path = require('path');
-const logger = require('morgan');
 const bodyParser = require('body-parser');
 const index = require('./routes/index');
 
 const app = express();
 
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
@@ -27,13 +24,14 @@ app.use((req, res, next) => {
 
 // error handler
 app.use((err, req, res, next) => {
+  console.log("error handler called...");
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.sendStatus(500);
+  res.sendStatus(err.status || 500);
+  // res.sendStatus(500);
 });
 
 exports.app = functions.https.onRequest(app);
