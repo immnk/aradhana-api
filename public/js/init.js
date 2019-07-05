f82c2334021e0d9dcb7a7fcacea34f999f1b9b81
Manikanta Tankala <im.manikanta@gmail.com>
2019-07-05 23:53:55 +0800

    fix submit and loading with alerts

--- public/js/init.js
+++ public/js/init.js
@@ -3,11 +3,13 @@
         $('.sidenav').sidenav();
         $('.datepicker').datepicker();
 
-        const LOCAL_END_POINT = "http://localhost:5001/aradhana-api/us-central1/app/";
-        const REMOTE_END_POINT = "https://us-central1-aradhana-api.cloudfunctions.net/app/";
+        const LOCAL_END_POINT = "http://localhost:5001/aradhana-api/us-central1/app";
+        const REMOTE_END_POINT = "https://us-central1-aradhana-api.cloudfunctions.net/app";
 
         $("#eventForm").on("submit", function (e) {
             e.preventDefault(); // cancel the actual submit
+
+            $("#submitButton").prop("disabled", true);
 
             generatePreview();
 
@@ -24,15 +26,16 @@
                 "url": `${LOCAL_END_POINT}/create`,
                 "method": "POST",
                 "headers": {
-                    "content-type": "application/json",
-                    "cache-control": "no-cache",
+                    "content-type": "application/json"
                 },
                 "processData": false,
                 "data": JSON.stringify(formData)
             }
 
             $.ajax(settings).done(function (response) {
+                $("#submitButton").prop("disabled", false);
                 console.log(response);
+                alert(response.message);
             });
 
             console.log(formData);
